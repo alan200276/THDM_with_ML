533550e02b56fd0f2ea4697b4fa7f22220073dd9
alan200276 <alan200276@gmail.com>
2022-03-24 14:35:15 +0800

    add script: batch sample simulation

---
+++ Cards/sample_flow/sample_flow.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+mg5cardpath="/home/alan/ML_Analysis/THDM/Cards/sample_flow"
+outpath="/home/alan/ML_Analysis/THDM/Cards/sample_flow/Log"
+mcdatapath="/home/u5/THDM/sample_flow"
+
+preprocesspath="/home/alan/ML_Analysis/THDM/Preprocess"
+downsizesavepath="/home/u5/THDM/sample_flow/Downsized"
+
+
+echo "Start Running"
+date
+
+
+# Madgraph Part
+
+rand=$RANDOM
+rand="$rand"1
+
+echo "Random Seed =  $rand "
+
+sed -i -e "s/randomseed/"$rand"/g" $mg5cardpath/proc_ppjjjj.txt 
+
+python /root/MG5_aMC_v2_7_3/bin/mg5_aMC $mg5cardpath/proc_ppjjjj.txt > $outpath/proc_ppjjjj_"$rand".log
+
+sed -i -e "s/"$rand"/randomseed/g" $mg5cardpath/proc_ppjjjj.txt 
+
+rm -rf $mcdatapath/proc_*/Events/run_*/*.hepmc.gz
+
+
+
+# Downsize Part
+
+python3 $preprocesspath/downsize.py $mcdatapath/proc_jjjj_"$rand"/Events/run_01/tag_1_delphes_events.root ppjjjj $downsizesavepath  $rand >  $outpath/ppjjjj_downsize_"$rand".log
+
+
+
+# Preprocess Part
+
+python3 $preprocesspath/preprocess_sample_flow.py $downsizesavepath/EventList_ppjjjj_"$rand".h5 ppjjjj $rand > $outpath/ppjjjj_preprocess_"$rand".log
+
+
+
+rm -rf $mcdatapath/proc_*
+
+echo "Finish"
+
+date
