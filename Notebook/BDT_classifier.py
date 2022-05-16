9a44f7e3d26bfa801e109e86a9f1f3e9d51b4dd3
alan200276 <alan200276@gmail.com>
2022-05-16 10:41:20 +0800

    save prediction to npy

--- Notebook/BDT_classifier.py
+++ Notebook/BDT_classifier.py
@@ -150,9 +150,12 @@ bkg_ttbar = pd.read_csv(path+"ttbar.csv")
 bkg_ppbbbb = pd.read_csv(path+"ppbbbb.csv")
 bkg_ppjjjb = pd.read_csv(path+"ppjjjb.csv")
 bkg_ppjjjj = pd.read_csv(path+"ppjjjj.csv")
+#%%
+logging.info(len(sig_ppHhh))
 
 #%%
 sig_ppHhh = Preselection(sig_ppHhh)
+logging.info(len(sig_ppHhh))
 #%%
 bkg_ttbar = Preselection(bkg_ttbar)
 bkg_ppbbbb = Preselection(bkg_ppbbbb)
@@ -167,12 +170,20 @@ bkg_ppbbbb["label"] = np.full(len(bkg_ppbbbb),0)
 bkg_ppjjjb["label"] = np.full(len(bkg_ppjjjb),0)
 bkg_ppjjjj["label"] = np.full(len(bkg_ppjjjj),0)
 
+# sig_ppHhh = shuffle(sig_ppHhh)[:312398]
+
 #%%
+
 Data = pd.concat([sig_ppHhh, bkg_ttbar], ignore_index=True, axis=0,join='inner')
 Data = pd.concat([Data, bkg_ppbbbb], ignore_index=True, axis=0,join='inner')
 
 # %%
 Data = Preselection(Data)
+
+logging.info("\n")
+logging.info("There are {} sig and {} bkg in dataset.".format(len(Data[Data["label"]==1]),len(Data[Data["label"]!=1])))
+logging.info("\n")
+
 #%%
 X = Data
 Y = Data["label"]
@@ -223,104 +234,25 @@ dump(clf_GBDT, "./clf_GBDT_1000_Xhh.h5")
 #%%
 clf_GBDT = load("./clf_GBDT_1000_Xhh.h5")
 #%%
-"""
-Prediction 
-"""
-sig_ppHhh["prediction"] = clf_GBDT.predict_proba(np.nan_to_num(sig_ppHhh[features]))[:,1]
-bkg_ttbar["prediction"] = clf_GBDT.predict_proba(np.nan_to_num(bkg_ttbar[features]))[:,1]
-bkg_ppbbbb["prediction"] = clf_GBDT.predict_proba(np.nan_to_num(bkg_ppbbbb[features]))[:,1]
-bkg_ppjjjb["prediction"] = clf_GBDT.predict_proba(np.nan_to_num(bkg_ppjjjb[features]))[:,1]
-bkg_ppjjjj["prediction"] = clf_GBDT.predict_proba(np.nan_to_num(bkg_ppjjjj[features]))[:,1]
 
-# %%
-sig_ppHhh_4_catal = sig_ppHhh[sig_ppHhh["four_b_tag"] == 1 ]
-bkg_ttbar_4_catal = bkg_ttbar[bkg_ttbar["four_b_tag"] == 1 ]
-bkg_ppbbbb_4_catal = bkg_ppbbbb[bkg_ppbbbb["four_b_tag"] == 1 ]
-bkg_ppjjjb_4_catal = bkg_ppjjjb[bkg_ppjjjb["four_b_tag"] == 1 ]
-bkg_ppjjjj_4_catal = bkg_ppjjjj[bkg_ppjjjj["four_b_tag"] == 1 ]
+process = {
+            "ppHhh" : sig_ppHhh,
+            "ttbar" : bkg_ttbar,
+            "ppbbbb" : bkg_ppbbbb,
+            "ppjjjb" : bkg_ppjjjb,
+            "ppjjjj" : bkg_ppjjjj,
+        }  
 
-#%%
-Luminosity = 3000 * (0.77**4)
-Xection_ppHhh = 0.81186/1000 #pb
-# Xection_ppHhh = 3.070/1000 #pb
-Xection_ttbar = 260.3554 *0.27515 #pb
-Xection_ppbbbb = 0.4070 #pb
-Xection_ppjjjb = 450.04 #pb
-Xection_ppjjjj = 11087.8358304 *0.02365529 #pb 
-BRHhh = 0.8715
-BRhbb = 0.3560
-# BRHhh = 0.9413
-# BRhbb = 0.3558
+for pro in process:
+    logging.info("Process: {}".format(pro))
+    logging.info("Data total length: {}".format(len(process[pro])))
 
+    prediction = clf_GBDT.predict_proba(process[pro][features])
+    np.save("./prediction/"+str(pro)+"_BDT_prediction_trail1000", prediction)
 
-
-#%%
-nset_simulation = 20
-# cut_threshold = 0.9925
-cut_threshold = 0.98#52972814739865
-
-sig_eff = len(sig_ppHhh_4_catal)/(nset_simulation*100000) 
-bkg_ttbar_eff = len(bkg_ttbar_4_catal)/(nset_simulation*100000)
-bkg_ppbbbb_eff = len(bkg_ppbbbb_4_catal)/(nset_simulation*100000)
-bkg_ppjjjb_eff = len(bkg_ppjjjb_4_catal)/(nset_simulation*100000)
-bkg_ppjjjj_eff = len(bkg_ppjjjj_4_catal)/(nset_simulation*100000+len(process_path_jjjj_2)*10000)
-
-
-logging.info("Efficiency Before BDT")
-logging.info("Efficiency in 4b Category")
-logging.info("sig_4_catal_eff : {} # of events: {:.2f}".format(sig_eff, sig_eff*Xection_ppHhh*BRHhh*BRhbb*BRhbb*Luminosity*1000))
-logging.info("bkg_ttbar_4_catal_eff : {} # of events: {:.2f}".format(bkg_ttbar_eff, bkg_ttbar_eff*Xection_ttbar*Luminosity*1000))
-logging.info("bkg_ppbbbb_4_catal_eff : {} # of events: {:.2f}".format(bkg_ppbbbb_eff, bkg_ppbbbb_eff*Xection_ppbbbb*Luminosity*1000))
-logging.info("bkg_ppjjjb_4_catal_eff : {} # of events: {:.2f}".format(bkg_ppjjjb_eff, bkg_ppjjjb_eff*Xection_ppjjjb*Luminosity*1000))
-logging.info("bkg_ppjjjj_4_catal_eff : {} # of events: {:.2f}".format(bkg_ppjjjj_eff, bkg_ppjjjj_eff*Xection_ppjjjj*Luminosity*1000))
-logging.info("\n")
-
-sig_eff = len(sig_ppHhh_4_catal[sig_ppHhh_4_catal["prediction"] > cut_threshold])/(nset_simulation*100000) 
-bkg_ttbar_eff = len(bkg_ttbar_4_catal[bkg_ttbar_4_catal["prediction"] > cut_threshold])/(nset_simulation*100000)
-bkg_ppbbbb_eff = len(bkg_ppbbbb_4_catal[bkg_ppbbbb_4_catal["prediction"] > cut_threshold])/(nset_simulation*100000)
-bkg_ppjjjb_eff = len(bkg_ppjjjb_4_catal[bkg_ppjjjb_4_catal["prediction"] > cut_threshold])/(nset_simulation*100000)
-bkg_ppjjjj_eff = len(bkg_ppjjjj_4_catal[bkg_ppjjjj_4_catal["prediction"] > cut_threshold])/(nset_simulation*100000+len(process_path_jjjj_2)*10000)
-
-logging.info("Efficiency After BDT")
-logging.info("Efficiency in 4b Category")
-logging.info("sig_4_catal_eff : {} # of events: {:.2f}".format(sig_eff, sig_eff*Xection_ppHhh*BRHhh*BRhbb*BRhbb*Luminosity*1000))
-logging.info("bkg_ttbar_4_catal_eff : {} # of events: {:.2f}".format(bkg_ttbar_eff, bkg_ttbar_eff*Xection_ttbar*Luminosity*1000))
-logging.info("bkg_ppbbbb_4_catal_eff : {} # of events: {:.2f}".format(bkg_ppbbbb_eff, bkg_ppbbbb_eff*Xection_ppbbbb*Luminosity*1000))
-logging.info("bkg_ppjjjb_4_catal_eff : {} # of events: {:.2f}".format(bkg_ppjjjb_eff, bkg_ppjjjb_eff*Xection_ppjjjb*Luminosity*1000))
-logging.info("bkg_ppjjjj_4_catal_eff : {} # of events: {:.2f}".format(bkg_ppjjjj_eff, bkg_ppjjjj_eff*Xection_ppjjjj*Luminosity*1000))
-logging.info("\n")
-# %%
-nset_simulation = 20
-# cut_threshold = 0.9948#70815049923983
-# cut_threshold = 0.9905#6164489563258
-cut_threshold = 0.98#52972814739865
-
-sig_eff = len(sig_ppHhh)/(nset_simulation*100000)
-bkg_ttbar_eff = len(bkg_ttbar)/(nset_simulation*100000)
-bkg_ppbbbb_eff = len(bkg_ppbbbb)/(nset_simulation*100000)
-bkg_ppjjjb_eff = len(bkg_ppjjjb)/(nset_simulation*100000)
-bkg_ppjjjj_eff = len(bkg_ppjjjj)/(nset_simulation*100000+len(process_path_jjjj_2)*10000)
-
-logging.info("Efficiency Before BDT")
-logging.info("sig_eff : {} # of events: {:.2f}".format(sig_eff, sig_eff*Xection_ppHhh*BRHhh*BRhbb*BRhbb*Luminosity*1000))
-logging.info("bkg_ttbar_eff : {} # of events: {:.2f}".format(bkg_ttbar_eff, bkg_ttbar_eff*Xection_ttbar*Luminosity*1000))
-logging.info("bkg_ppbbbb_eff : {} # of events: {:.2f}".format(bkg_ppbbbb_eff, bkg_ppbbbb_eff*Xection_ppbbbb*Luminosity*1000))
-logging.info("bkg_ppjjjb_eff : {} # of events: {:.2f}".format(bkg_ppjjjb_eff, bkg_ppjjjb_eff*Xection_ppjjjb*Luminosity*1000))
-logging.info("bkg_ppjjjj_eff : {} # of events: {:.2f}".format(bkg_ppjjjj_eff, bkg_ppjjjj_eff*Xection_ppjjjj*Luminosity*1000))
-logging.info("\n")
-
-
-sig_eff = len(sig_ppHhh[sig_ppHhh["prediction"] > cut_threshold])/(nset_simulation*100000) 
-bkg_ttbar_eff = len(bkg_ttbar[bkg_ttbar["prediction"] > cut_threshold])/(nset_simulation*100000)
-bkg_ppbbbb_eff = len(bkg_ppbbbb[bkg_ppbbbb["prediction"] > cut_threshold])/(nset_simulation*100000)
-bkg_ppjjjb_eff = len(bkg_ppjjjb[bkg_ppjjjb["prediction"] > cut_threshold])/(nset_simulation*100000)
-bkg_ppjjjj_eff = len(bkg_ppjjjj[bkg_ppjjjj["prediction"] > cut_threshold])/(nset_simulation*100000+len(process_path_jjjj_2)*10000)
-
-logging.info("Efficiency After BDT")
-logging.info("sig_eff : {} # of events: {:.2f}".format(sig_eff, sig_eff*Xection_ppHhh*BRHhh*BRhbb*BRhbb*Luminosity*1000))
-logging.info("bkg_ttbar_eff : {} # of events: {:.2f}".format(bkg_ttbar_eff, bkg_ttbar_eff*Xection_ttbar*Luminosity*1000))
-logging.info("bkg_ppbbbb_eff : {} # of events: {:.2f}".format(bkg_ppbbbb_eff, bkg_ppbbbb_eff*Xection_ppbbbb*Luminosity*1000))
-logging.info("bkg_ppjjjb_eff : {} # of events: {:.2f}".format(bkg_ppjjjb_eff, bkg_ppjjjb_eff*Xection_ppjjjb*Luminosity*1000))
-logging.info("bkg_ppjjjj_eff : {} # of events: {:.2f}".format(bkg_ppjjjj_eff, bkg_ppjjjj_eff*Xection_ppjjjj*Luminosity*1000))
-logging.info("\n")
+    logging.info("Shape of prediction {}".format(prediction.shape))
+    logging.info("Prediction total length: {}".format(len(prediction)))
+    
+    logging.info("{}".format("Done!"))
+    logging.info("\n")
 # %%
