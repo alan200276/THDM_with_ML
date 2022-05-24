f4f17708fc6ec87ed67513dc17fb4d4cb86c8f83
alan200276 <alan200276@gmail.com>
2022-05-24 14:07:08 +0800

    update

--- Notebook/function.py
+++ Notebook/function.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 import numpy as np
 import pandas as pd
+import time
+from tqdm import tqdm
+from tensorflow.keras.utils import to_categorical
 import importlib
 import logging
 
@@ -92,3 +95,83 @@ def Basic_Preselection(data : pd.DataFrame)-> pd.DataFrame:
 
 
     return data
+
+
+"""
+Define Generator
+"""
+
+def data_generator(imagepath: str, data_dict: pd.DataFrame, nb_samples: int, batch_size: int):
+    while True:
+        for start in range(0, nb_samples, batch_size):
+            x_leanding_jet_batch = []
+            x_subleanding_jet_batch = []
+            x_rotated_event_batch = []
+            y_batch = []
+
+            end = min(start + batch_size, nb_samples)
+            for img_index in range(start, end):
+                
+                x_train_path = imagepath + data_dict["Image"].iloc[img_index]
+
+                x_train_leanding_jet = np.load(x_train_path)["leading_jet_image"]
+                x_train_leanding_jet = np.nan_to_num(x_train_leanding_jet)
+                x_leanding_jet_batch.append(x_train_leanding_jet)
+
+                x_train_subleanding_jet = np.load(x_train_path)["subleading_jet_image"]
+                x_train_subleanding_jet = np.nan_to_num(x_train_subleanding_jet)
+                x_subleanding_jet_batch.append(x_train_subleanding_jet)
+
+                x_train_rotated_event = np.load(x_train_path)["rotated_event_image"]
+                x_train_rotated_event = np.nan_to_num(x_train_rotated_event)
+                x_rotated_event_batch.append(x_train_rotated_event)
+        
+                
+                if data_dict["Y"].iloc[img_index] == 0:
+                    y_batch.append(["0"])
+                if data_dict["Y"].iloc[img_index] != 0:
+                     y_batch.append(["1"])
+
+            yield ([np.asarray(x_leanding_jet_batch), np.asarray(x_subleanding_jet_batch), np.asarray(x_rotated_event_batch)], to_categorical(np.asarray(y_batch)))
+
+
+"""
+Define Collector
+"""
+
+def loading_data(imagepath: str, data_dict: pd.DataFrame, start: int=0, stop: int=20000): 
+    x_leanding_jet = []
+    x_subleanding_jet = []
+    x_rotated_event = []
+    y = []
+
+    logging.info("Collect Data from {} to {}.".format(start,stop))
+    time.sleep(0.5)
+    for img_index in tqdm(range(start,stop)):
+        try:
+            x_train_path = imagepath + data_dict["Image"].iloc[img_index]
+
+            x_train_leanding_jet = np.load(x_train_path)["leading_jet_image"]
+            x_train_leanding_jet = np.nan_to_num(x_train_leanding_jet)
+            x_leanding_jet.append(x_train_leanding_jet)
+
+            x_train_subleanding_jet = np.load(x_train_path)["subleading_jet_image"]
+            x_train_subleanding_jet = np.nan_to_num(x_train_subleanding_jet)
+            x_subleanding_jet.append(x_train_subleanding_jet)
+
+            x_train_rotated_event = np.load(x_train_path)["rotated_event_image"]
+            x_train_rotated_event = np.nan_to_num(x_train_rotated_event)
+            x_rotated_event.append(x_train_rotated_event)
+
+            # x_jet_tmp = np.divide((x_jet_tmp - norm_dict[0]), (np.sqrt(norm_dict[1])+1e-5))#[0].reshape(1,40,40)
+            if data_dict["Y"].iloc[img_index] == 0:
+                y.append(["0"])
+            if data_dict["Y"].iloc[img_index] != 0:
+                y.append(["1"])
+        except:
+            break
+
+        # if img_index == stop:
+        #     break
+
+    return [np.asarray(x_leanding_jet), np.asarray(x_subleanding_jet), np.asarray(x_rotated_event)], to_categorical(np.asarray(y))
